03d3a6e5dbee7e878453360b66b1e7e699b43bb1
glt <2022679249@qq.com>
2026-02-25 18:01:08 +0800

    Split Components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,135 +1,7 @@
 <template>
   <div class="mainlayoutt">
     <div class="header">
-      <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-        :ellipsis="false"
-        @select="handleSelect"
-      >
-        <el-menu-item index="1">
-          <el-icon><Monitor /></el-icon>首页
-        </el-menu-item>
-        <el-menu-item index="2"> 番剧 </el-menu-item>
-        <el-menu-item index="3"> 直播 </el-menu-item>
-        <el-menu-item index="4"> 游戏中心 </el-menu-item>
-        <el-menu-item index="5"> 会员购 </el-menu-item>
-        <el-menu-item index="6"> 漫画 </el-menu-item>
-        <el-menu-item index="7"> 赛事 </el-menu-item>
-        <el-menu-item index="8"> 新春会 </el-menu-item>
-        <el-menu-item index="9">
-          <el-icon><Download /></el-icon>下载客户端
-        </el-menu-item>
-        <el-menu-item index="10" style="flex-grow: 2; max-width: 300px">
-          <el-input
-            v-model="input1"
-            style="width: 100%"
-            size="large"
-            placeholder="Please Input"
-            :suffix-icon="Search"
-          />
-        </el-menu-item>
-        <el-menu-item>
-          <el-avatar :src="urlAvatar" />
-        </el-menu-item>
-        <el-menu-item>
-          <div
-            class="topbottom"
-            style="
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              line-height: normal;
-            "
-          >
-            <el-icon><Tools /></el-icon>
-            <span>大会员</span>
-          </div>
-        </el-menu-item>
-        <el-menu-item>
-          <div
-            class="topbottom"
-            style="
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              line-height: normal;
-            "
-          >
-            <el-icon><Tools /></el-icon>
-            <span>消息</span>
-          </div>
-        </el-menu-item>
-        <el-menu-item>
-          <div
-            class="topbottom"
-            style="
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              line-height: normal;
-            "
-          >
-            <el-icon><Tools /></el-icon>
-            <span>动态</span>
-          </div>
-        </el-menu-item>
-        <el-menu-item>
-          <div
-            class="topbottom"
-            style="
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              line-height: normal;
-            "
-          >
-            <el-icon><Tools /></el-icon>
-            <span>收藏</span>
-          </div>
-        </el-menu-item>
-        <el-menu-item>
-          <div
-            class="topbottom"
-            style="
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              line-height: normal;
-            "
-          >
-            <el-icon><Tools /></el-icon>
-            <span>历史</span>
-          </div>
-        </el-menu-item>
-        <el-menu-item>
-          <div
-            class="topbottom"
-            style="
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              line-height: normal;
-            "
-          >
-            <el-icon><Tools /></el-icon>
-            <span>创作中心</span>
-          </div>
-        </el-menu-item>
-        <el-menu-item>
-          <el-button type="danger">
-            <el-icon><Tools /></el-icon>
-            投稿
-          </el-button>
-        </el-menu-item>
-      </el-menu>
+      <header-nav/>
     </div>
     <div class="mainlayout">
       <div class="nav">
@@ -161,10 +33,10 @@
 
 <script setup>
 import { ref } from 'vue'
-import header from '@/assets/avatar.png'
 import Bg from '@/assets/bg.png'
 import Channel from './Channel.vue'
 import Carousel from './Carousel.vue'
+import HeaderNav from './HeaderNav.vue'
 console.log(Bg)
 // 初始视频列表
 const videoList = ref([
@@ -182,7 +54,6 @@ const videoList = ref([
   // ...
 ])
 
-const urlAvatar = header
 const urlBg = Bg
 const fit = 'cover'
 const isLoading = ref(false)
@@ -224,35 +95,9 @@ const handleSelect = (key, keyPath) => {
   width: 100%;
   height: 100%;
 }
-/* 强制所有菜单项可以收缩 */
-:deep(.el-menu-item) {
-  flex-shrink: 1; /* 允许收缩 */
-  min-width: 0; /* 允许缩小到极值 */
-  padding: 0 5px !important; /* 进一步压缩间距 */
-  font-size: 13px; /* 稍微调小字号 */
-  display: flex;
-  align-items: center;
-}
 
-/* 移除鼠标悬浮时的背景色 */
-:deep(.el-menu-item:hover) {
-  background-color: transparent !important;
-}
 
-/* 如果你点击菜单项后，也不想要那个选中的背景色，可以加上下面这段 */
-:deep(.el-menu-item.is-active) {
-  background-color: transparent !important;
-}
-/* 针对带有图标和文字垂直排列的容器 */
-.topbottom {
-  flex-shrink: 1;
-  min-width: 0;
-}
 
-.topbottom span {
-  font-size: 14px; /* 垂直排列时文字可以更小 */
-  transform: scale(0.9); /* 甚至可以用缩放 */
-}
 
 .mainlayoutt {
   width: 100%;
@@ -269,27 +114,7 @@ const handleSelect = (key, keyPath) => {
   min-width: 1200px
 }
 
-.el-menu-demo {
-  width: 98%; /* 只有内容区域是 80% */
-  margin: 0 auto; /* 居中内容 */
-  border-bottom: none !important; /* 去掉自带边框，改用 header 的边框 */
-  background-color: transparent !important;
-  border: none !important;
-  display: flex;
-  justify-content: space-between;
-  width: 90%; /* 保持与 .mainlayout 一致的宽度 */
-  max-width: 100%;
-  min-width: 1200px; /* 让菜单和下方的 Grid 同步产生滚动条，而不是互相挤压 */
-}
-.el-menu-item {
-  color: white;
-}
-/* 1. 缩小每个菜单项的左右内边距 */
-:deep(.el-menu--horizontal > .el-menu-item) {
-  padding: 0 8px !important; /* 进一步压缩 */
-  min-width: auto !important; /* 强制取消最小宽度限制 */
-  flex-shrink: 1;             /* 确保每一项都能参与收缩 */
-}
+
 
 /* 2. 针对带图标和文字垂直排列的特殊项，进一步压缩 */
 .topbottom {
